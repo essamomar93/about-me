f7987208cd1329c42a9046c552af81c361c3c58a
essam omar <essam.93@hotmail.com>
2021-07-07 10:24:45 +0300

    code format repair

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ class 2:
 we lern how to add if statment and use inside it || && 
 
 class 3: 
-today we lern alot about the loobs and where and when we can use it . 
+today we lern alot about the loops where and when we can use it .
+and lern about aray and how to add it for the loop . 
+
 
 
 
--- js/app.js
+++ js/app.js
@@ -11,113 +11,139 @@ let score = 0;
 
 let firstq = prompt('If i told you i\'am Mechanical Engineer Do you think i\'am understod everything in the Cars ?');
 
-firstq=firstq.toLowerCase(); 
+firstq = firstq.toLowerCase();
 // console.log(firstq); 
 
-if(firstq==='yes'||firstq==='y'){
-        // console.log("no thats wrong");
-        alert("no thats wrong");
-}else if (firstq==='no'||firstq==='n'){
-        // console.log("you are right");
-        alert("you are right");
-        score++;   
+if (firstq === 'yes' || firstq === 'y') {
+    // console.log("no thats wrong");
+    alert("no thats wrong");
+} else if (firstq === 'no' || firstq === 'n') {
+    // console.log("you are right");
+    alert("you are right");
+    score++;
 
 }
-let secondq = prompt("Now if i till you iam studed for 12 yesr school and 6 year uni do you think iam 30 years old? " )
+let secondq = prompt("Now if i till you iam studed for 12 yesr school and 6 year uni do you think iam 30 years old? ")
 
-secondq=secondq.toLowerCase();
+secondq = secondq.toLowerCase();
 
-if(secondq==='yes'||secondq==='y'){
-        // console.log("you must try to calculate it again iam 28");
-        alert("you must try to calculate it again iam 28");
-} else if(secondq==='no'||secondq==='n'){
+if (secondq === 'yes' || secondq === 'y') {
+    // console.log("you must try to calculate it again iam 28");
+    alert("you must try to calculate it again iam 28");
+} else if (secondq === 'no' || secondq === 'n') {
 
-        // console.log("correct iam just 28");
-        alert("sure iam just 28");
-        score++; 
+    // console.log("correct iam just 28");
+    alert("sure iam just 28");
+    score++;
 }
 
-let therdq =prompt("do you think that i have work now ?")
+let therdq = prompt("do you think that i have work now ?")
 
-therdq=therdq.toLowerCase();
+therdq = therdq.toLowerCase();
 
-if(therdq==='yes'||therdq==='y') {
+if (therdq === 'yes' || therdq === 'y') {
 
-        // console.log("if i have work i can't take this course");
-        alert("if i have work i can't take this course");
-} else if(therdq==='no'||therdq==='n'){
+    // console.log("if i have work i can't take this course");
+    alert("if i have work i can't take this course");
+} else if (therdq === 'no' || therdq === 'n') {
 
-        // console.log("Unfortunately i don't have");
-        alert("Unfortunately i don't have");
-        score++; 
-
-}
-let forthq =prompt("do i look like summer lover? ")
-
-forthq=forthq.toLowerCase();
-
-if(forthq==='yes'||forthq==='y'){
-        // console.log("wrong anser i hate the summer");
-        alert("wrong anser i hate the summer");
-} else if(forthq==='no'||forthq==='n'){
-
-        // console.log("sure i'm in love with the winter");
-        alert("sure i'm in love with the winter");
-        score++; 
-}
-
-let fifthq =prompt("If i told you iam 28 years do you think my care is older than me ?")
-
-fifthq=fifthq.toLowerCase();
-
-if(fifthq==='yes'||fifthq==='y') {
-
-            // console.log("yes i have V.Wmodel 1967");
-            alert("yes i have V.Wmodel 1967");
-            score++; 
-}else if(fifthq==='no'||fifthq==='n') {
-
-        // console.log("wrong anser , i have V.W model 1967");
-        alert("wrong anser , i have V.W model 1967");
+    // console.log("Unfortunately i don't have");
+    alert("Unfortunately i don't have");
+    score++;
 
 }
+let forthq = prompt("do i look like summer lover? ")
+
+forthq = forthq.toLowerCase();
+
+if (forthq === 'yes' || forthq === 'y') {
+    // console.log("wrong anser i hate the summer");
+    alert("wrong anser i hate the summer");
+} else if (forthq === 'no' || forthq === 'n') {
+
+    // console.log("sure i'm in love with the winter");
+    alert("sure i'm in love with the winter");
+    score++;
+}
+
+let fifthq = prompt("If i told you iam 28 years do you think my care is older than me ?")
+
+fifthq = fifthq.toLowerCase();
+
+if (fifthq === 'yes' || fifthq === 'y') {
+
+    // console.log("yes i have V.Wmodel 1967");
+    alert("yes i have V.Wmodel 1967");
+    score++;
+} else if (fifthq === 'no' || fifthq === 'n') {
+
+    // console.log("wrong anser , i have V.W model 1967");
+    alert("wrong anser , i have V.W model 1967");
+
+}
+let sixth = "";
+let corect = 7;
+
 for (let i = 0; i < 4; i++) {
-    let sixth = prompt("lets guess number betwen 1-10 that i put it in my minde ? ")
+    sixth = prompt("lets guess number betwen 1-10 that i put it in my minde ? ");
+    sixth = parseInt(sixth);
 
-    sixth = sixth.toLowerCase();
 
-    if (sixth === '7') {
+
+    if (sixth === 7) {
         alert("you are right")
         score++;
         break;
-    } else if (sixth === '8' || sixth === '9' || sixth === '10') {
-        alert("too hight");
-    } else if (sixth === '6' || sixth === '5' || sixth === '4' || sixth === '3' || sixth === '2' || sixth === '1') {
-        alert("too low");
+
+    }
+    else if (sixth < corect) {
+        alert("too law");
+
+    } else if (sixth > corect) {
+        alert("too hight")
+
     }
     else {
-        alert("its not a number betwen 1-10");
+        alert("pleas wright number")
     }
 }
 alert("my number was 7");
 
-
+let siventh = "";
+let favcar = ['volkswagen', 'bmw', 'tesla', 'toyota'];
 car:
 for (let i = 0; i < 6; i++) {
-    let siventh = prompt("let guess one of my favoret cars type ? ");
-
-    let favcar = ['volkswagen', 'bmw', 'tesla', 'toyota'];
-
+    siventh = prompt("let guess one of my favoret cars type ? ");
+    siventh = siventh.toLowerCase();
     // console.log(favcar);
-   
+
     for (let j = 0; j < favcar.length; j++) {
         if (siventh == favcar[j]) {
             alert("you guess it ");
             // console.log(favcar[j]);
-            score++; 
+            score++;
             break car;
 
-            } 
+        }
+    }
+    if (i=== 0) {
+        alert ("You still have 5 options")
+        
+    }else if (i === 1) {
+        alert ("You still have 4 options")
+
+    }else if (i === 2) {
+        alert ("You still have 3 options")
+
+    }else if (i === 3) {
+        alert ("You still have 2 options")
+
+    }else if (i === 4) {
+        alert ("You still have 1 options")
+
+    }else if (i === 5) {
+        alert ("You dont have more options")
+
     }
 
 }
@@ -126,5 +152,4 @@ alert("my favoret cars is  'volkswagen', 'bmw', 'tesla', 'toyota'");
 // console.log(score);
 alert("you got score " + score + "/7");
 
-alert("thank you " + username + " for playing this game"); 
-
+alert("thank you " + username + " for playing this game");
